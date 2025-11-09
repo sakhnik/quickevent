69747aecd166e7568e85d10a53634708b29004ee
Anatolii Sakhnik <sakhnik@gmail.com>
2025-11-09 20:19:42 +0200

    Select cp1251 in Xprinter t80q

--- quickevent/app/quickevent/plugins/Receipts/src/receiptsprinter.cpp
+++ quickevent/app/quickevent/plugins/Receipts/src/receiptsprinter.cpp
@@ -431,7 +431,7 @@ static const QByteArray epson_commands[] =
 	QByteArray("\033@"),			//    Init
 	QByteArray("\035V\102\x00", 4),	//    Cut
 	QByteArray("\n"),				//    Eoln
-	QByteArray("\033t\x06", 3),		//    SelectCodeTable1251
+	QByteArray("\x1c\x2e\x1b\x52\x00\x1bt\x17", 8),		//    SelectCodeTable1251
 	zelesta_logo
 };
 
